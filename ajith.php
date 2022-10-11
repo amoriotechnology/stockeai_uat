cba835d612a4e4efb275ecdce1b0f4c925ba7714
Ajith-RRR <Ajith@amoriotech.com>
2022-10-11 18:21:02 +0530

    ajith

--- ajith.php
+++ ajith.php
@@ -1,8 +1,19 @@
 <?php 
 echo "hello";
 echo "Ajithkumar";
+    // //supplier list
+    // public function supplier_list() {
+    //     $this->db->select('*');
+    //     $this->db->from('supplier_information');
+    //     $this->db->where('created_by',$this->session->userdata('user_id'));
+    //     $this->db->order_by('supplier_id', 'desc');
+    //     $query = $this->db->get();
+    //     if ($query->num_rows() > 0) {
+    //         return $query->result_array();
+    //     }
+    //     return false;
+    // }
 
-
-
-
+    echo "ajith";
+    echo "kumar";
 ?>
